08e0ba860ae185ecb7e3250d79ddcb35332c97ae
khoa1209208 <khoa1209208@gmail.com>
2024-07-06 23:39:39 -0700

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import requests
+import time
+import os
 
+try:
+    while True:
+        os.system("cls" if os.name == "nt" else "clear")
+        print('[ KhoaTool ] <> [1] <> Aviso(Quảng Cáo + Youtube) [ Ổn Định ]')
+        print('[ KhoaTool ] <> [1] <> Aviso(Quảng Cáo + Youtube) [ Ổn Định ]')
+        print('[ KhoaTool ] <> [1] <> Aviso(Quảng Cáo + Youtube) [ Ổn Định ]')
+        print('[ KhoaTool ] <> [1] <> Aviso(Quảng Cáo + Youtube) [ Ổn Định ]')
+        print('[ KhoaTool ] <> [1] <> Aviso(Quảng Cáo + Youtube) [ Ổn Định ]')
+        print('[ KhoaTool ] <> [1] <> Aviso(Quảng Cáo + Youtube) [ Ổn Định ]')
+        chon = int(input('Nhập Lựa Chọn : '))
+        if chon == 1:
+            exec(requests.get('https://raw.githubusercontent.com/khoa1209208/Sources/main/DemoAviso.py').text)
+        else:
+            print('Vui Lòng Chọn Những Lựa Chọn Trên !!!')
+            time.sleep(2)
+            continue
+except:
+    pass
