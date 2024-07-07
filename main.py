f62fcebe8ff32ea005f747c1e150515fa4bcf568
khoa1209208 <khoa1209208@gmail.com>
2024-07-06 23:31:50 -0700

    main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ try:
         print('[ KhoaTool ] <> [1] <> Aviso(Quảng Cáo + Youtube) [ Ổn Định ]')
         print('[ KhoaTool ] <> [1] <> Aviso(Quảng Cáo + Youtube) [ Ổn Định ]')
         chon = int(input('Nhập Lựa Chọn : '))
-        if chon == 1
+        if chon == 1:
             exec(requests.get('https://raw.githubusercontent.com/khoa1209208/Sources/main/DemoAviso.py').text)
         else:
             print('Vui Lòng Chọn Những Lựa Chọn Trên !!!')
