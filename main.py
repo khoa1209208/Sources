1de51d457ec3ccabd4c4236c37689dfd07334eb4
khoa1209208 <khoa1209208@gmail.com>
2024-07-10 19:58:55 -0700

    main.py

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ try:
         print('[ KhoaTool ] </> [1] Aviso (Quảng Cáo + Youtube) [ Ổn Định ]')
         chon = int(input('Nhập Lựa Chọn : '))
         if chon == 1:
+            os.system("cls" if os.name == "nt" else "clear")
             exec(requests.get('https://raw.githubusercontent.com/khoa1209208/Sources/main/DemoAviso.py').text)
+        if chon == 2:
+            os.system("cls" if os.name == "nt" else "clear")
+            exec(requests.get('https://raw.githubusercontent.com/khoa1209208/Sources/main/enc_pepe.py').text)
         else:
+            os.system("cls" if os.name == "nt" else "clear")
             print('Vui Lòng Chọn Những Lựa Chọn Trên !!!')
             time.sleep(2)
             continue
